f7728212405ba15d00d4e39f4c7684234c993192
YevheniiaUhnich <140949326+YevheniiaUhnich@users.noreply.github.com>
2025-03-16 11:09:16 +0200

    update

--- src/js/covers.js
+++ src/js/covers.js
@@ -1,5 +1,11 @@
 const coverSection = document.querySelector('.cover');
 const contentSection = document.querySelector('.content');
+const rows = document.querySelectorAll('.row');
+
+
+rows.forEach((row, index) => {
+  row.style.backgroundImage = `url('images/image${index + 1}.jpg')`; 
+  row.style.animationDelay = `${index * 0.5}s`; });
 
 const observer = new IntersectionObserver((entries) => {
   if (entries[0].isIntersecting) {
